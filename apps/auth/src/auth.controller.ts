4c90514be3d7085d71ff27d323b00b3494798881
Chibuikem Victor Okaneme <okanemechibuikem@gmail.com>
2023-11-01 19:48:25 +0000

    initial setup

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,16 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
-import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
-  }
-
   @MessagePattern({ cmd: 'get-user' })
   async getUser(@Ctx() ctx: RmqContext) {
     const channel = ctx.getChannelRef();
